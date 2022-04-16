47a29729f21fbc7af6f8e6311a6311304c41eddd
aggelen <aggelen@outlook.com>
2022-04-16 18:58:56 +0300

    boot sector experiments

---
+++ boot/boot.asm
@@ -0,0 +1,60 @@
+mov ah, 0x0e
+mov al, 'k'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'u'
+int 0x10
+
+mov ah, 0x0e
+mov al, 't'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'O'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'S'
+int 0x10
+
+mov ah, 0x0e
+mov al, '-'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'A'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'l'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'i'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'v'
+int 0x10
+
+mov ah, 0x0e
+mov al, 'e'
+int 0x10
+
+mov ah, 0x0e
+mov al, '.'
+int 0x10
+
+mov ah, 0x0e
+mov al, '.'
+int 0x10
+
+mov ah, 0x0e
+mov al, '.'
+int 0x10
+jmp $
+
+times 510-($-$$) db 0
+db 0x55, 0xaa
+
